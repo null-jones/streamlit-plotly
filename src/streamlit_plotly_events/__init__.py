cd643623bb3b38fd27435678a17583c5b71a4e27
jeff <jeffaw99@hotmail.com>
2024-07-03 00:18:15 +0800

    added measure function

--- src/streamlit_plotly_events/__init__.py
+++ src/streamlit_plotly_events/__init__.py
@@ -8,7 +8,7 @@ from json import loads
 # release process.)
 _RELEASE = True
 
-# Declare a Streamlit component. `declare_component` returns a function
+# Declare a Streamlit component. `declare_component` returns a  function
 # that is used to create instances of the component. We're naming this
 # function "_component_func", with an underscore prefix, because we don't want
 # to expose it directly to users. Instead, we will create a custom wrapper
@@ -24,7 +24,7 @@ if not _RELEASE:
         # We give the component a simple, descriptive name ("my_component"
         # does not fit this bill, so please choose something better for your
         # own component :)
-        "plotly_events",
+        "my_component",
         # Pass `url` here to tell Streamlit that the component will be served
         # by the local dev server that you run via `npm run start`.
         # (This is useful while your component is in development.)
@@ -32,11 +32,11 @@ if not _RELEASE:
     )
 else:
     # When we're distributing a production version of the component, we'll
-    # replace the `url` param with `path`, and point it to to the component's
+    # replace the `url` param with `path`, and point it to the component's
     # build directory:
     parent_dir = os.path.dirname(os.path.abspath(__file__))
     build_dir = os.path.join(parent_dir, "frontend/build")
-    _component_func = components.declare_component("plotly_events", path=build_dir)
+    _component_func = components.declare_component("my_component", path=build_dir)
 
 
 # Create a wrapper function for the component. This is an optional
@@ -44,7 +44,7 @@ else:
 # `declare_component` and call it done. The wrapper allows us to customize
 # our component's API: we can pre-process its input args, post-process its
 # output value, and add a docstring for users.
-def plotly_events(
+def my_component(
     plot_fig,
     click_event=True,
     with_z=False,
@@ -53,23 +53,17 @@ def plotly_events(
     override_height=450,
     override_width="100%",
     key=None,
+    plot_clicked_point: bool = False,
+    clicked_point_size: float = 0.0,
+    measure_mode: bool = False
 ):
-    """Create a new instance of "plotly_events".
+    """Create a new instance of "my_component".
 
     Parameters
     ----------
-    plot_fig: Plotly Figure
-        Plotly figure that we want to render in Streamlit
-    click_event: boolean, default: True
-        Watch for click events on plot and return point data when triggered
-    select_event: boolean, default: False
-        Watch for select events on plot and return point data when triggered
-    hover_event: boolean, default: False
-        Watch for hover events on plot and return point data when triggered
-    override_height: int, default: 450
-        Integer to override component height.  Defaults to 450 (px)
-    override_width: string, default: '100%'
-        String (or integer) to override width.  Defaults to 100% (whole width of iframe)
+    name: str
+        The name of the thing we're saying hello to. The component will display
+        the text "Hello, {name}!"
     key: str or None
         An optional key that uniquely identifies this component. If this is
         None, and the component's arguments are changed, the component will
@@ -77,25 +71,13 @@ def plotly_events(
 
     Returns
     -------
-    list of dict
-        List of dictionaries containing point details (in case multiple overlapping
-        points have been clicked).
-
-        Details can be found here:
-            https://plotly.com/javascript/plotlyjs-events/#event-data
-
-        Format of dict:
-            {
-                x: float (x value of point),
-                y: float (y value of point),
-                z: float (z value of point), # optional enabled using with_z
-                curveNumber: (index of curve),
-                pointNumber: (index of selected point),
-                pointIndex: (index of selected point)
-            }
+    int
+        The number of times the component's "Click Me" button has been clicked.
+        (This is the value passed to `Streamlit.setComponentValue` on the
+        frontend.)
 
     """
-    # kwargs will be exposed to frontend in "args"
+
     component_value = _component_func(
         plot_obj=plot_fig.to_json(),
         override_height=override_height,
@@ -105,49 +87,12 @@ def plotly_events(
         with_z=with_z,
         select_event=select_event,
         hover_event=hover_event,
-        default="[]",  # Default return empty JSON list
+        default="[]",
+        plot_clicked_point=plot_clicked_point,
+        clicked_point_size=clicked_point_size,
+        measure_mode = measure_mode,
     )
 
-    # Parse component_value since it's JSON and return to Streamlit
-    return loads(component_value)
-
-
-# Add some test code to play with the component while it's in development.
-# During development, we can run this just as we would any other Streamlit
-# app: `$ streamlit run src/streamlit_plotly_events/__init__.py`
-if not _RELEASE:
-    import streamlit as st
-    import plotly.express as px
-
-    st.set_page_config(layout="wide")
-
-    st.subheader("Plotly Line Chart")
-    fig = px.line(x=[0, 1, 2, 3], y=[0, 1, 2, 3])
-    plot_name_holder = st.empty()
-    clickedPoint = plotly_events(fig, key="line")
-    plot_name_holder.write(f"Clicked Point: {clickedPoint}")
-
-    # Here we add columns to check auto-resize/etc
-    st.subheader("Plotly Bar Chart (With columns)")
-    _, c2, _ = st.columns((1, 6, 1))
-    with c2:
-        fig2 = px.bar(x=[0, 1, 2, 3], y=[0, 1, 2, 3])
-        plot_name_holder2 = st.empty()
-        clickedPoint2 = plotly_events(fig2, key="bar")
-        plot_name_holder2.write(f"Clicked Point: {clickedPoint2}")
-
-    st.subheader("# Plotly Select Event")
-    fig3 = px.bar(x=[0, 1, 2, 3], y=[0, 1, 2, 3])
-    plot_name_holder3 = st.empty()
-    clickedPoint3 = plotly_events(
-        fig3, key="select", click_event=False, select_event=True
-    )
-    plot_name_holder3.write(f"Selected Point: {clickedPoint3}")
-
-    st.subheader("# Plotly Hover Event")
-    fig4 = px.bar(x=[0, 1, 2, 3], y=[0, 1, 2, 3])
-    plot_name_holder4 = st.empty()
-    clickedPoint4 = plotly_events(
-        fig4, key="hover", click_event=False, hover_event=True
-    )
-    plot_name_holder4.write(f"Hovered Point: {clickedPoint4}")
+    # We could modify the value returned from the component if we wanted.
+    # There's no need to do this in our simple example - but it's an option.
+    return component_value
--- src/streamlit_plotly_events/frontend/src/StreamlitPlotlyEventsComponent.tsx
+++ src/streamlit_plotly_events/frontend/src/StreamlitPlotlyEventsComponent.tsx
@@ -5,41 +5,54 @@ import {
 } from "streamlit-component-lib";
 import React, { ReactNode } from "react";
 import Plot from 'react-plotly.js';
-import { trace } from "console";
 
-class StreamlitPlotlyEventsComponent extends StreamlitComponentBase {
+interface MyState {
+  data: any[];
+  layout: any;
+  clickedPoints: { x: number; y: number; z: number };
+  measureMode: boolean;
+  measurePoints: any[];
+}
+
+class StreamlitPlotlyEventsComponent extends StreamlitComponentBase<MyState> {
   // Create state for points
-  state = {
+  state: MyState = {
     data: [],
     layout: {},
-    frames: [],
-    config: {}
+    clickedPoints: { x: 0.0, y: 0.0, z: 0.0 },
+    measureMode: false,
+    measurePoints: [],
+
   };
 
+  // init the component state when first starting it
+  // make the layout constant across updates
   componentDidMount() {
-    var plot_obj = JSON.parse(this.props.args["plot_obj"]);
+    let plot_obj = JSON.parse(this.props.args["plot_obj"])
+    let measure_mode: boolean = this.props.args['measure_mode']
     this.setState({
       data: plot_obj.data,
-      layout: plot_obj.layout,
-      frames: plot_obj.frames,
-      config: plot_obj.config
+      layout: { uirevision: 'constant' },
+      clickedPoints: {
+        x: 0,
+        y: 0,
+        z: 0,
+      },
+      measureMode: measure_mode,
+      measurePoints: [],
     });
   }
 
   public render = (): ReactNode => {
     // Pull Plotly object from args and parse
     var plot_obj = JSON.parse(this.props.args["plot_obj"]);
-
-    // this.setState({
-    //   data: plot_obj.data,
-    //   layout: plot_obj.layout,
-    //   frames: plot_obj.frames,
-    //   config: plot_obj.config,
-    // })
-
     const override_height = this.props.args["override_height"];
     const override_width = this.props.args["override_width"];
-
+    const plotClickedPoint: boolean = this.props.args['plot_clicked_point']
+    /*Get the current point size of the chart we are plotting to draw the extra clicked point
+      with respect to the scale of the plotted points
+    */
+    const clickedPointSize: number = this.props.args['clicked_point_size']
     // Event booleans
     const click_event = this.props.args["click_event"];
     const select_event = this.props.args["select_event"];
@@ -51,9 +64,9 @@ class StreamlitPlotlyEventsComponent extends StreamlitComponentBase {
       <Plot
         data={this.state.data}
         layout={this.state.layout}
-        config={this.state.config}
-        frames={this.state.frames}
-        onClick={click_event ? this.plotlyEventHandler(with_z, plot_obj) : undefined}
+        // config={this.state.config}
+        // frames={this.state.frames}
+        onClick={click_event ? this.plotlyEventHandler(with_z, plot_obj, plotClickedPoint, clickedPointSize) : undefined}
         onSelected={select_event ? this.plotlyEventHandler(with_z) : undefined}
         onHover={hover_event ? this.plotlyEventHandler(with_z) : undefined}
         style={{ width: override_width, height: override_height }}
@@ -63,7 +76,7 @@ class StreamlitPlotlyEventsComponent extends StreamlitComponentBase {
   };
 
   /** Click handler for plot. */
-  private plotlyEventHandler = (with_z: boolean, plot_obj?: any) => {
+  private plotlyEventHandler = (with_z: boolean, plot_obj?: any, plot_clicked_point?: boolean, clicked_point_size?: number) => {
 
     // console.log('pressed on point')
     return (data: any) => {
@@ -82,51 +95,234 @@ class StreamlitPlotlyEventsComponent extends StreamlitComponentBase {
       // JSON.parse(data.points[0])
       // console.log('pressed on point', JSON.stringify(data.points[0], getCircularReplacer(), 2))
       // console.log('current plot object', plot_obj)
-      const getPoint = data.points[0]
-      const traceIndex = getPoint.curveNumber
-      const pointIndex = getPoint.pointNumber
+      let currentClickedPoint = data.points[0]
+      let previousClickedPointState = this.state.clickedPoints
+      let needToUpdate = false
+      if (currentClickedPoint.x !== previousClickedPointState.x ||
+        currentClickedPoint.y !== previousClickedPointState.y ||
+        currentClickedPoint.z !== previousClickedPointState.z) {
+        console.log('clicked point x diff')
+        needToUpdate = true
+        this.setState({
+          clickedPoints: {
+            x: currentClickedPoint.x,
+            y: currentClickedPoint.y,
+            z: currentClickedPoint.z,
+          }
+        })
+      }
+      // console.log('state point: ', this.state.clickedPoints)
+      // console.log('clicked point: ',  currentClickedPoint.x)
+      if (plot_clicked_point && needToUpdate === true) {
+        needToUpdate = false
+        const getPoint = data.points[0]
+        const traceIndex = getPoint.curveNumber
+        const pointIndex = getPoint.pointNumber
+        console.log(`current getPoint:`, getPoint)
+        // console.log('current point number:', pointIndex)
+        // console.log('current trace number', traceIndex)
+        // console.log('current colors: ', plot_obj.data[traceIndex].marker.color[pointIndex])
+        // console.log('current data for plot', this.state.data)
 
-      // console.log(plot_obj)
-      // console.log('current point number:', pointIndex)
-      // console.log('current trace number', traceIndex)
-      // console.log('current colors: ', plot_obj.data[traceIndex].marker.color[pointIndex])
-      // console.log('current point size',plot_obj.data[traceIndex].marker)
-      // set the color by modifying the state of data here
-
-      // const newData = plot_obj
-      plot_obj.data[traceIndex].marker.color[pointIndex] = 'red'
-      
-      this.setState({
-        data:plot_obj.data
-      })
-      
-      // Build array of points to return
-      var clickedPoints: Array<any> = [];
-      data.points.forEach(function (arrayItem: any) {
-        if (with_z) {
-          clickedPoints.push({
-            x: arrayItem.x,
-            y: arrayItem.y,
-            z: arrayItem.z,
-            curveNumber: arrayItem.curveNumber,
-            pointNumber: arrayItem.pointNumber,
-            pointIndex: arrayItem.pointIndex
-          });
-        } else {
-          clickedPoints.push({
-            x: arrayItem.x,
-            y: arrayItem.y,
-            curveNumber: arrayItem.curveNumber,
-            pointNumber: arrayItem.pointNumber,
-            pointIndex: arrayItem.pointIndex
-          });
+        // console.log('plot_obj', plot_obj)
+        // create a point object to add to our current plot to indicate the clicked point
+        interface newPoint {
+          x: number[];
+          y: number[];
+          z?: number[];
+          mode: string;
+          marker: {
+            color: string;
+            size?: number;
+          };
+          showlegend: boolean;
+          type: string;
+          name: string,
+          text: string[],
+          hovertemplate: string,
         }
-      });
 
-      // Return array as JSON to Streamlit
-      Streamlit.setComponentValue(JSON.stringify(clickedPoints));
+        let clickPointPlot: newPoint = {
+          x: [getPoint.x],
+          y: [getPoint.y],
+          z: [getPoint.z],
+          mode: 'markers',
+          marker: {
+            color: 'red',
+            // size: clicked_point_size === 0.0 && undefined? 5.0: clicked_point_size*1.5,
+          },
+          showlegend: false,
+          type: getPoint.z !== undefined ? 'scatter3d' : 'scatter',
+          name: 'Clicked Point',
+          text: ['Clicked Point'],
+          hovertemplate: '<b>%{text}</b><br>'
+          + `<b>x: ${getPoint.x.toFixed(3)}</b><br>`
+          + `<b>y: ${getPoint.y.toFixed(3)}</b><br>`
+          + `<b>z: ${getPoint.z.toFixed(3)}</b><extra></extra>`
+
+        };
+
+        if (clicked_point_size !== undefined) {
+          clickPointPlot.marker.size = clicked_point_size
+        }
+        else {
+          clickPointPlot.marker.size = 5.0
+        }
+
+
+        // calculate measure points and plot them on the chart
+        if (this.state.measureMode === true) {
+          // let measurePoints = this.state.measurePoints
+          // measurePoints.push(clickPointPlot)
+          if (this.state.measurePoints.length >= 2) {
+            this.state.measurePoints.length = 0;
+            this.state.measurePoints.push(clickPointPlot)
+          }
+          else {
+            this.state.measurePoints.push(clickPointPlot)
+          }
+          // calculate measure points when the length is 2 then plot on the chart
+          if (this.state.measurePoints.length === 2) {
+            const measurePoints = this.state.measurePoints
+
+            const measurePlot = this.createMeasureLine(measurePoints)
+            measurePoints.push(...measurePlot)
+          }
+
+          console.log('measure points: ', this.state.measurePoints)
+        }
+        // update the plot 
+        // append new trace of clicked point here then update plot state
+        if (this.state.measureMode === true) {
+          plot_obj.data.push(...this.state.measurePoints)
+        }
+        else {
+          plot_obj.data.push(clickPointPlot)
+        }
+        console.log('plotting using: ', plot_obj)
+        this.updatePlotState(plot_obj)
+      }
+
+      // Build array of points to return
+      if (needToUpdate === true) {
+        let clickedPoints: Array<any> = [];
+        data.points.forEach(function (arrayItem: any) {
+          if (with_z) {
+            clickedPoints.push({
+              x: arrayItem.x,
+              y: arrayItem.y,
+              z: arrayItem.z,
+              curveNumber: arrayItem.curveNumber,
+              pointNumber: arrayItem.pointNumber,
+              pointIndex: arrayItem.pointIndex
+            });
+          } else {
+            clickedPoints.push({
+              x: arrayItem.x,
+              y: arrayItem.y,
+              curveNumber: arrayItem.curveNumber,
+              pointNumber: arrayItem.pointNumber,
+              pointIndex: arrayItem.pointIndex
+            });
+          }
+        });
+
+        // Return array as JSON to Streamlit
+        Streamlit.setComponentValue(JSON.stringify(clickedPoints));
+      }
+
     };
   };
+  private createMeasureLine = (measurePoints: any) => {
+    const dx = Math.abs(measurePoints[1].x - measurePoints[0].x)
+    const dy = Math.abs(measurePoints[1].y - measurePoints[0].y)
+    const dz = Math.abs(measurePoints[1].z - measurePoints[0].z)
+    const dxyz = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2) + Math.pow(dz, 2))
+
+    const dxy = Math.sqrt(dx * dx + dy * dy);
+    const dxz = Math.sqrt(dx * dx + dz * dz);
+    const dyz = Math.sqrt(dy * dy + dz * dz);
+
+    // xyz measure line
+    const linexyz = {
+      x: [...measurePoints[0].x, ...measurePoints[1].x],
+      y: [...measurePoints[0].y, ...measurePoints[1].y],
+      z: [...measurePoints[0].z, ...measurePoints[1].z],
+      mode: 'lines',
+      line: {
+        color: 'purple',
+        width: 4,
+      },
+      type: 'scatter3d',
+      name: 'Distance',
+      text: ["Point 1", "Point 2"],
+      hovertemplate: "<b>%{text}</b><br>"
+        + `<b>Measured distance: ${dxyz.toFixed(3)}m</b><br>`
+        + `<b>ΔXY: ${dxy.toFixed(3)}</b><br>`
+        + `<b>ΔXZ: ${dxz.toFixed(3)}</b><br>`
+        + `<b>ΔYZ: ${dyz.toFixed(3)}</b><extra></extra>`
+    };
+
+    // x measure line
+    const linex = {
+      x: [...measurePoints[0].x, ...measurePoints[1].x],
+      y: [...measurePoints[0].y, ...measurePoints[0].y],
+      z: [...measurePoints[0].z, ...measurePoints[0].z],
+      mode: 'lines',
+      line: {
+        color: 'red',
+        width: 4,
+      },
+      type: 'scatter3d',
+      name: 'Distance X',
+      text: ["Point 1", "Point 2"],
+      hovertemplate: `<b>Measured distance X: ${dx.toFixed(3)}m</b><extra></extra>`
+    };
+
+    // xz measure line
+    const liney = {
+      x: [...measurePoints[1].x, ...measurePoints[1].x],
+      y: [...measurePoints[0].y, ...measurePoints[1].y],
+      z: [...measurePoints[0].z, ...measurePoints[0].z],
+      mode: 'lines',
+      line: {
+        color: 'green',
+        width: 4,
+      },
+      type: 'scatter3d',
+      name: 'Distance Y',
+      text: ["Point 1", "Point 2"],
+      hovertemplate: `<b>Measured distance Y: ${dy.toFixed(3)}m</b><extra></extra>`
+    };
+
+    // z meassure line
+    const linez = {
+      x: [...measurePoints[1].x, ...measurePoints[1].x],
+      y: [...measurePoints[1].y, ...measurePoints[1].y],
+      z: [...measurePoints[0].z, ...measurePoints[1].z],
+      mode: 'lines',
+      line: {
+        color: 'blue',
+        width: 4,
+      },
+      type: 'scatter3d',
+      name: 'Distance Z',
+      text: ["Point 1", "Point 2"],
+      hovertemplate: `<b>Measured distance X: ${dz.toFixed(3)}m</b><extra></extra>`
+    };
+    return (
+      [linexyz, linex, liney, linez]
+    )
+  }
+  private updatePlotState = (plot_data: any): void => {
+    this.setState({
+      data: plot_data.data,
+      // layout: plot_data.layout,
+      // config: this.state.config,
+      // frames: this.state.frames,
+
+    })
+  }
 }
 
 export default withStreamlitConnection(StreamlitPlotlyEventsComponent);
